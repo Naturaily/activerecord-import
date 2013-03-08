66635ef024184f5b2f3bdfe17779a1b3ea731c50
Przemek Lusar <lluzak@gmail.com>
2013-03-08 17:38:22 +0100

    added support for returning row ids

--- lib/activerecord-import/adapters/abstract_adapter.rb
+++ lib/activerecord-import/adapters/abstract_adapter.rb
@@ -1,75 +1,78 @@
 module ActiveRecord::Import::AbstractAdapter
   NO_MAX_PACKET = 0
   QUERY_OVERHEAD = 8 #This was shown to be true for MySQL, but it's not clear where the overhead is from.
-    
+
   module ClassMethods
     def get_insert_value_sets( values, sql_size, max_bytes ) # :nodoc:
-      value_sets = []          
+      value_sets = []
       arr, current_arr_values_size, current_size = [], 0, 0
       values.each_with_index do |val,i|
         comma_bytes = arr.size
-        sql_size_thus_far = sql_size + current_size + val.bytesize + comma_bytes
+        sql_size_thus_far = sql_size + current_size + val.size + comma_bytes
         if NO_MAX_PACKET == max_bytes or sql_size_thus_far <= max_bytes
-          current_size += val.bytesize            
+          current_size += val.bytesize
           arr << val
         else
           value_sets << arr
           arr = [ val ]
           current_size = val.bytesize
         end
-      
+
         # if we're on the last iteration push whatever we have in arr to value_sets
         value_sets << arr if i == (values.size-1)
       end
       [ *value_sets ]
     end
   end
-  
+
   module InstanceMethods
     def next_value_for_sequence(sequence_name)
       %{#{sequence_name}.nextval}
     end
-  
-    # +sql+ can be a single string or an array. If it is an array all 
+
+    # +sql+ can be a single string or an array. If it is an array all
     # elements that are in position >= 1 will be appended to the final SQL.
     def insert_many( sql, values, *args ) # :nodoc:
       # the number of inserts default
       number_of_inserts = 0
-    
+      row_ids = []
+
       base_sql,post_sql = if sql.is_a?( String )
         [ sql, '' ]
       elsif sql.is_a?( Array )
         [ sql.shift, sql.join( ' ' ) ]
       end
-    
-      sql_size = QUERY_OVERHEAD + base_sql.size + post_sql.size 
+
+      sql_size = QUERY_OVERHEAD + base_sql.size + post_sql.size
 
       # the number of bytes the requested insert statement values will take up
       values_in_bytes = values.sum {|value| value.bytesize }
-    
+
       # the number of bytes (commas) it will take to comma separate our values
       comma_separated_bytes = values.size-1
-    
+
       # the total number of bytes required if this statement is one statement
       total_bytes = sql_size + values_in_bytes + comma_separated_bytes
-    
+
       max = max_allowed_packet
-    
+
       # if we can insert it all as one statement
-      if NO_MAX_PACKET == max or total_bytes < max
+      sql_result = if NO_MAX_PACKET == max or total_bytes < max
         number_of_inserts += 1
-        sql2insert = base_sql + values.join( ',' ) + post_sql
-        insert( sql2insert, *args )
+        sql2insert = base_sql + values.join( ',' ) + post_sql + " RETURNING ID"
+        execute( sql2insert, *args )
       else
         value_sets = self.class.get_insert_value_sets( values, sql_size, max )
         value_sets.each do |values|
           number_of_inserts += 1
-          sql2insert = base_sql + values.join( ',' ) + post_sql
-          insert( sql2insert, *args )
+          sql2insert = base_sql + values.join( ',' ) + post_sql + " RETURNING ID"
+          execute( sql2insert, *args )
         end
       end
 
-      number_of_inserts
+      row_ids = sql_result.map(&:values).flatten
+
+      [number_of_inserts, row_ids]
     end
 
     def pre_sql_statements(options)
